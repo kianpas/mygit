1a5ec639e8e7c95ceafaf7f33c921d224b45845e
kianpas <kianpas@gmail.com>
2021-05-03 17:06:28 +0900

    food menu 추가

--- 05_hellogit/src/com/kh/git/food/FoodServlet.java
+++ 05_hellogit/src/com/kh/git/food/FoodServlet.java
@@ -27,7 +27,7 @@ public class FoodServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("깁밥 좋아요~").append("떡복이 i love it!");
 	}
 
 }
